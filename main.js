db5c43c73764495cd3c8cd7b85e7e66da8280118
poprer656sad <chouerzi@gmail.com>
2020-07-21 21:16:24 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,116 +1,122 @@
-const electron = require('electron')
-const { app, BrowserWindow, protocol, net } = electron;
-const fs = require('fs');
-const moment = require('moment');
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+puppeteer.use(StealthPlugin());
 const express = require('express');
 const bodyParser = require('body-parser');
-const { performance } = require('perf_hooks');
-const request = require('request-promise');
-const crypto = require("crypto");
-const SocksProxyAgent = require('socks-proxy-agent');
+const url = require('url');
+const ghostCursor = require("ghost-cursor");
+const adyenEncrypt = require('node-adyen-encrypt')(18);
 var util = require('util');
+var page;
+const page_list = ['https://www.footlocker.com/product/model/starter-sweatshirt-womens/318828.html','https://www.footlocker.com/product/model/starter-sweatshirt-womens/318828.html','https://www.footlocker.com/product/model/nike-satin-hook-t-shirt-mens/323947.html','https://www.footlocker.com/product/model/reebok-vector-crew-mens/318979.html','https://www.footlocker.com/product/model/converse-all-star-ox-boys-grade-school/191450.htm','https://www.footlocker.com/product/model/converse-all-star-ox-womens/149982.html','https://www.footlocker.com/product/model/nike-air-force-1-low-boys-grade-school/100214.html','https://www.footlocker.com/product/nike-air-fear-of-god-moc-mens/M8086200.html', 'https://footlocker.com', 'https://www.footlocker.com/product/nike-lebron-17-low-mens/D5007101.html']
 
-var expressApp, bankExpressApp, bankServer;
-
-var captchaBank = [];
-
-function sleep(ms) {
-	return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-app.on('ready', () => {
-	initCaptchaWindow();
+puppeteer.launch({ headless: true, args: [`--window-size=1366,768`] }).then(async browser => {
+  page = await browser.newPage();
+  page.setViewport({width: 1366, height:728})
+  await page.goto('https://footlocker.com');
+  await page.evaluate(()=>{
+    bmak.fpcf.fpValstr = "1454252292;895908107;dis;,7,8;true;true;true;240;true;24;24;true;false;-1";
+    window.innerHeight = 657;
+    window.innerWidth = 1366;
+//    window.screen.height = 768;
+  })
+  const cursor = ghostCursor.createCursor(page);
+    setInterval(async function(){
+    try{await cursor.move('body');
+    if(Math.random() > 0.85){await cursor.click();}
+    }catch(e){}}, 0.5)
 })
 
-async function initCaptchaWindow() {
-	captchaWindow = new BrowserWindow({
-		width: 480,
-		height: 680,
-			webPreferences: {
-				allowRunningInsecureContent: true
-			}
-	})
+async function check_cookies(){
+    var tstart = 0;
+    while(true){
+        var cookies = await page.cookies();
+        for(i = 0; i < cookies.length; i++){
+            if(cookies[i].name == '_abck'){
+                console.log(cookies[i].value);
+                if ((cookies[i].value.slice(-12).includes('=~') || cookies[i].value.slice(-12).includes('||')) && !cookies[i].value.slice(-12).includes('==')){
+                    return cookies[i].value;
+                }
+            }
+        }
+        tstart ++;
+        if(tstart > 5){
+            await page.goto(page_list[parseInt(Math.random()*11)])
+            tstart = 0;
+        }
+        await new Promise(r => setTimeout(r, 2000));
+    }
+}
 
-	SetupIntercept();
+async function abck(abckcookie, link, indx){
+    const cookie = {
+          name: '_abck',
+          value: abckcookie,
+          domain: '.footlocker.com',
+          url: 'https://www.footlocker.com/',
+          path: '/',
+          httpOnly: false,
+          secure: true
+        }
+    await page.setCookie(cookie);
+    await page.evaluate((link, indx) => {
+        history.pushState({}, null, link);
+        bmak.apicall_bm(bmak.cf_url, true, bmak.patp);
+    }, link, indx);
+    return await check_cookies()
+}
 
-	captchaWindow.loadURL('https://accounts.google.com');
-	
-	await sleep(1000)
-	
-	captchaWindow.on('close', function(e){
-		captchaWindow = null;
-	});
+async function sensor(abckcookie, link, indx){
+    const cookie = {
+          name: '_abck',
+          value: abckcookie,
+          domain: '.footlocker.com',
+          url: 'https://www.footlocker.com/',
+          path: '/',
+          httpOnly: false,
+          secure: true
+        }
 
-	captchaWindow.webContents.session.webRequest.onBeforeRequest({urls: ['https://myaccount.google.com/*']}, (details, callback) => {
-		callback({redirectURL: 'http://supremenewyork.com/'})
-	})
-};
+    await page.setCookie(cookie);
+    return await page.evaluate((link, indx) => {
+        history.pushState({}, null, link);
+        bmak.cma(MouseEvent, 1);
+        bmak.cdma(DeviceMotionEvent);
+        bmak.aj_type = 1;
+        bmak.aj_index = indx;
+        bmak.bpd();
+        return bmak.sensor_data;
+    }, link, indx);
+}
 
-function SetupIntercept() {
-	protocol.interceptBufferProtocol('http', (req, callback) => {
-		if(req.url == 'http://supremenewyork.com/') {
-			fs.readFile(__dirname + '/captcha.html', 'utf8', function(err, html){
-				callback({mimeType: 'text/html', data: Buffer.from(html)});
-			});
-		}else{
-			const request = net.request(req)
-			request.on('response', res => {
-				const chunks = []
-
-				res.on('data', chunk => {
-					chunks.push(Buffer.from(chunk))
-				})
-
-				res.on('end', async () => {
-					const file = Buffer.concat(chunks)
-					callback(file)
-				})
-			})
-
-			if (req.uploadData) {
-				req.uploadData.forEach(part => {
-					if (part.bytes) {
-						request.write(part.bytes)
-					} else if (part.file) {
-						request.write(readFileSync(part.file))
-					}
-				})
-			}
-
-			request.end()
-		}
-	})
-};
-
-electron.ipcMain.on('openCapWindow', function(event, args) {
-    initCaptchaWindow();
-});
-
-electron.ipcMain.on('sendCaptcha', function(event, token) {
-
-	captchaBank.push({
-	  token: token,
-	  timestamp: moment(),
-	  host: 'http://supremenewyork.com/',
-	  sitekey: '6LeWwRkUAAAAAOBsau7KpuC9AV-6J8mhw4AjC3Xz'
-	})
-});
-
-setInterval(function(){
-	for (var i = 0; i < captchaBank.length; i++) {
-
-  	if (moment().diff(moment(captchaBank[i].timestamp), 'seconds') > 110) {
-		console.log('Removing Expired Captcha Token')
-		captchaBank.splice(0,1)
-
-	  }
-	}
-}, 1000);
+async function adyen_encrypt(adyenkey, card_num, card_month, card_year, card_cvv, card_name){
+     const options = {};
+     const cardData = {
+         number : card_num,       // 'xxxx xxxx xxxx xxxx'
+         cvc : card_cvv,                 //'xxx'
+         holderName : card_name,   // 'John Doe'
+         expiryMonth : card_month, //'MM'
+         expiryYear : card_year,   // 'YYYY'
+         generationtime : new Date().toISOString() // new Date().toISOString()
+     };
+     const cseInstance = adyenEncrypt.createEncryption(adyenkey, options);
+     console.log(cseInstance.validate({number: card_num, generationtime : new Date().toISOString()}));
+     console.log(cseInstance.validate({cvc : card_cvv, generationtime : new Date().toISOString()}));
+     console.log(cseInstance.validate({expiryMonth : card_month, generationtime : new Date().toISOString()}));
+     console.log(cseInstance.validate({expiryYear : card_year, generationtime : new Date().toISOString()}));
+     var encrypteddata = {"encryptedCardNumber":cseInstance.encrypt({number: card_num, generationtime : new Date().toISOString()}),
+        "encryptedExpiryMonth":cseInstance.encrypt({expiryMonth : card_month, generationtime : new Date().toISOString()}),
+        "encryptedExpiryYear":cseInstance.encrypt({expiryYear : card_year, generationtime : new Date().toISOString()}),
+        "encryptedSecurityCode":cseInstance.encrypt({cvc : card_cvv, generationtime : new Date().toISOString()})
+     }
+     console.log(encrypteddata);
+     return encrypteddata;
+}
 
 function initBankServer() {
-	bankExpressApp = express()
+	bankExpressApp = express();
 
-	let port = '8000';
+	let port = '7000';
 	let address = '127.0.0.1';
 
 	console.log('Bank server listening on port: ' + port);
@@ -119,15 +125,29 @@ function initBankServer() {
 	bankExpressApp.use(bodyParser.json());
 	bankExpressApp.use(bodyParser.urlencoded({ extended: true }));
 
-	bankExpressApp.get('/trigger', function(req, res) {
-		initCaptchaWindow();
-	});
+	bankExpressApp.get('/sensor', async function(req, res) {
+	    var forminf = url.parse(req.url, true).query;
+        res.send(await sensor(forminf.abck, forminf.url, forminf.indx));
+        res.end();
+    });
 
-	bankExpressApp.get('/fetch', function(req, res) {
-		return res.json(captchaBank),
-		captchaBank.splice(0,1);
-	});
+    bankExpressApp.get('/akamai', async function(req, res){
+        var forminf = url.parse(req.url, true).query;
+       res.send(await abck(forminf.abck, forminf.url, forminf.indx));
+       res.end();
+    });
 
+    bankExpressApp.get('/alternatepage', async function(req, res){
+       await page.goto(page_list[parseInt(Math.random()*11)])
+       res.send('reloaded');
+       res.end();
+    });
+
+    bankExpressApp.get('/adyen', async function(req, res){
+        var forminf = url.parse(req.url, true).query;
+        res.send(await adyen_encrypt(forminf.adyen_key, forminf.number, forminf.month, forminf.year, forminf.cvv, forminf.name));
+        res.end();
+    });
 
 	bankServer = bankExpressApp.listen(bankExpressApp.get('port'), bankExpressApp.get('address'));
 	}
